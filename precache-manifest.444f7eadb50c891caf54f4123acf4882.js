50652d567d7c8686cbaf0a5255d1f3b6023b3f6b
Volodsher <volodsher@gmail.com>
2019-09-25 15:56:32 +0300

    Updates

---
+++ precache-manifest.444f7eadb50c891caf54f4123acf4882.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9398d2d4c5b5c8029544d47f033f063f",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "408b8febc6cb4affdfcb",
+    "url": "/react_dynamic-list-of-posts/static/css/main.e9591aea.chunk.css"
+  },
+  {
+    "revision": "7ec1f218572b21831b2d",
+    "url": "/react_dynamic-list-of-posts/static/js/2.06308628.chunk.js"
+  },
+  {
+    "revision": "408b8febc6cb4affdfcb",
+    "url": "/react_dynamic-list-of-posts/static/js/main.175c18d2.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
